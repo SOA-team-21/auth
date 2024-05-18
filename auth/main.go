d2bc67a0756256b713d32166beab7775e0efa9a9
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2024-05-18 13:02:00 +0200

    fix: Changed name of host

--- auth/main.go
+++ auth/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func initDB() *gorm.DB {
-	dsn := "user=postgres password=super dbname=soa_auth host=soa_auth port=5432 sslmode=disable"
+	dsn := "user=postgres password=super dbname=soa_auth host=auth-database port=5432 sslmode=disable"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
